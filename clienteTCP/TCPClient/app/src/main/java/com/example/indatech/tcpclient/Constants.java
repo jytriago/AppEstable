ef70b58a18d0a060a3f72df696915ee8494d548d
jytriago <ytriago26@gmail.com>
2019-01-13 10:04:35 -0400

    Init Commit

---
+++ clienteTCP/TCPClient/app/src/main/java/com/example/indatech/tcpclient/Constants.java
@@ -0,0 +1,8 @@
+package com.example.indatech.tcpclient;
+
+public class Constants {
+
+    public static final String CLOSED_CONNECTION = "client_closed_connection";
+    public static final String LOGIN_NAME = "client_login_name";
+
+}
